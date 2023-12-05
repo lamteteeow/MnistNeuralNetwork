ca665ec32e113100f85720dee04e55d43a38d5b9
zy69guqi <richard.angersbach@fau.de>
2023-12-05 17:39:08 +0100

    Adapt scripts

--- mnist.sh
+++ mnist.sh
@@ -1,1 +1,11 @@
-echo "This script should trigger the training and testing of your neural network implementation..."
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <input_config>" >&2
+  exit 1
+fi
+
+input_config=$1
+
+export NUM_CORES=$(nproc --all)
+OMP_NUM_THREADS=$NUM_CORES ./build/app/mnist $input_config
