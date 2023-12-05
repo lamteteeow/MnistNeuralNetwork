3a286c265334117e5b095f96e647aa952a085537
zy69guqi <richard.angersbach@fau.de>
2023-12-05 17:39:08 +0100

    Adapt scripts

--- read_dataset_images.sh
+++ read_dataset_images.sh
@@ -1,1 +1,12 @@
-echo "This script should read a dataset image into a tensor and pretty-print it into a text file..."
+#!/bin/bash
+
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 <dataset_rel_path> <image_tensor_out_rel_path> <image_index>" >&2
+  exit 1
+fi
+
+dataset_rel_path=$1
+image_tensor_out_rel_path=$2
+image_index=$3
+
+./build/test/read_dataset_images "$dataset_rel_path" "$image_tensor_out_rel_path" "$image_index"
